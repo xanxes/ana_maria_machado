f28ac469cf0d7037c6de3d4c9e7fce024d332106
Marcelo Sanches <98404480+xanxes@users.noreply.github.com>
2022-08-20 17:21:17 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -29,7 +29,7 @@ const checkEndGame = () => {
 
   if (disabledCards.length === 20) {
     clearInterval(this.loop);
-    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
+    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML} segundos`);
   }
 }
 
